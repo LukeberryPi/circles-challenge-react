111fb277860fd04d6743fa3f0a3b24795a7635b2
Luke Berry <lukeberrypi@gmail.com>
2023-10-18 16:53:42 -0300

    add event listener for user clicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Coordinates = {
+  x: number;
+  y: number;
+};
 
 function App() {
-  return <p>hello world!</p>
+  const [clickCoordinates, setClickCoordinates] = useState<Coordinates>({ x: 0, y: 0 });
+
+  const registerCoordinates = (event: React.MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = event;
+    setClickCoordinates({ x: clientX, y: clientY });
+  };
+
+  return (
+    <div onClick={(e) => registerCoordinates(e)} className="wrapper">
+      <p>last X of click: {clickCoordinates.x}</p>
+      <p>last Y of click: {clickCoordinates.y}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
